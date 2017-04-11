d23d075cb0d3eecf8b7151dbbd60cea4d7b27c55
ideaportal <ideaportal@gmail.com>
2017-04-11 18:37:15 +0530

    changed roots.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#jmpress'
   get 'solutions' => "welcome#solutions"
+  # get 'foo' => "welcome#jmpress"
+  # get 'solutions' => "welcome#solutions"
+  get 'circular' => "welcome#circle"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
