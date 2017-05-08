f1a383e51f418d6efc98326cf35761239e60efc4
Achal Kaundinya <achal@idea-portal.co>
2017-05-08 15:25:54 +0530

    Created the circular slides page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,17 @@
 class WelcomeController  < ApplicationController
+
   def index
 
   end
+
   def jmpress
     @contact = Contact.new
   end
+
   def solutions
 
+  end
+  def circle
+    @contact = Contact.new
   end
 end
